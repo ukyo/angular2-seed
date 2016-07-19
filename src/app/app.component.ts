3d10da6becf3016befbf2af39c37b53e41597766
PatrickJS <github@gdi2290.com>
2016-07-19 10:21:02 -0700

    chore: router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
 import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 
 @Component({
+  selector: 'home',
+  template: 'Home'
+})
+export class Home {
+  constructor() {
+  }
+}
+
+@Component({
   selector: 'app',
+  directives: [
+    ...ROUTER_DIRECTIVES
+  ],
   template: `
   <navbar>
     <h1>Hello Angular 2 and Webpack 2</h1>
   </navbar>
 
   <main>
+    <a href="#" [routerLink]="['']">Home</a>
+    <a href="#" [routerLink]="['about']">About</a>
 
     <div>Your Content Here</div>
+    <router-outlet></router-outlet>
 
   </main>
 
@@ -22,3 +38,4 @@ export class App {
     console.log('Hello Angular 2 Webpack 2');
   }
 }
+
