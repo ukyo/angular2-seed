c2e8ac5b9b49516c1f0e9b8142bccee00a168759
PatrickJS <github@gdi2290.com>
2016-07-21 02:24:41 -0700

    fix: router loading

--- src/app/about.ts
+++ src/app/about.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'home',
-  template: 'Home'
+  selector: 'about',
+  template: 'About'
 })
-export class Home {
+export class About {
   constructor() {
   }
 }
