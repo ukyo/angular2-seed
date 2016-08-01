2da790b0cece9e9dca9ecd6dc4efe201b06c2cc8
PatrickJS <github@gdi2290.com>
2016-08-01 03:19:12 -0700

    refactor: create ngModuleExport

--- src/app/home/index.ts
+++ src/app/home/index.ts
@@ -1,1 +1,12 @@
+import {exportNgModules} from '../ng-module-helpers';
+
+import {Home} from './home';
+
 export * from './home';
+export default exportNgModules({
+  entryComponent: Home,
+  routes: [],
+  directives: [],
+  providers: [],
+  pipes: []
+});
